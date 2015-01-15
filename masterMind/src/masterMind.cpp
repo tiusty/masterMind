2d0e441324edd578212cfb0483676a55bb683925
Alex Agudelo <awagud@hotmail.com>
2015-01-15 14:33:25 -0500

    Adding cpp file to github

---
+++ masterMind/src/masterMind.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : masterMind.cpp
+// Author      : Alex Agudelo, Thurston Brevette
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
